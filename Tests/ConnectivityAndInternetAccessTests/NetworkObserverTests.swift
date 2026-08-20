a2d0e88e4a0ad9b936e708f4311ed72d94fa6365
Rodrigo Sambade <43375046+rodrigosambadesaa@users.noreply.github.com>
2026-08-20 11:13:57 +0200

    Use thread-safe TestBox in NetworkObserverTests for Sendable closure compliance

--- Tests/ConnectivityAndInternetAccessTests/NetworkObserverTests.swift
+++ Tests/ConnectivityAndInternetAccessTests/NetworkObserverTests.swift
@@ -1,6 +1,28 @@
 import XCTest
 @testable import ConnectivityAndInternetAccess
 
+final class TestBox<T>: @unchecked Sendable {
+    private let lock = NSLock()
+    private var _value: T
+
+    init(_ value: T) {
+        self._value = value
+    }
+
+    var value: T {
+        get {
+            lock.lock()
+            defer { lock.unlock() }
+            return _value
+        }
+        set {
+            lock.lock()
+            _value = newValue
+            lock.unlock()
+        }
+    }
+}
+
 final class NetworkObserverTests: XCTestCase {
 
     func testSnapshotNetworkStateReturnsValidObject() {
@@ -13,23 +35,23 @@ final class NetworkObserverTests: XCTestCase {
         let observer = NetworkObserver()
         let expectation = XCTestExpectation(description: "Initial passive state delivered")
 
-        var receivedState: NetworkState? = nil
+        let stateBox = TestBox<NetworkState?>(nil)
         let token = observer.observeNetwork { state in
-            receivedState = state
+            stateBox.value = state
             expectation.fulfill()
         }
 
         wait(for: [expectation], timeout: 2.0)
-        XCTAssertNotNil(receivedState)
+        XCTAssertNotNil(stateBox.value)
 
         token.close()
         XCTAssertTrue(token.isCancelled)
     }
 
     func testTokenCancellationIdempotency() {
-        var cancelCount = 0
+        let cancelCountBox = TestBox<Int>(0)
         let token = NetworkObserverToken {
-            cancelCount += 1
+            cancelCountBox.value += 1
         }
 
         XCTAssertFalse(token.isCancelled)
@@ -39,6 +61,6 @@ final class NetworkObserverTests: XCTestCase {
         // Multiple calls to cancel should be idempotent
         token.cancel()
         token.close()
-        XCTAssertEqual(cancelCount, 1)
+        XCTAssertEqual(cancelCountBox.value, 1)
     }
 }
